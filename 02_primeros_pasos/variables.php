9fa58664fcdd027296be923031f14b6978d24cb3
Kevin Omar Cabanillas Nuñez <kevinomar37@hotmail.com>
2020-03-17 08:07:00 -0700

    Definir Variables
    indicaciones para definir variables

---
+++ 02_primeros_pasos/variables.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Nombres de variables
+ * 
+ * deben iniciar con signo de $
+ * Seguido de una letra o _ 
+ * Pueden contener letras(alfabeto ingles), numeros y _
+ * No se puede repetir
+ * Distingue mayúsculas y minúsculas(Case Sensitive)
+ * No se puede utilizar palabras reservadas
+ * Se recomienda utilizar nombres que tengan sentido
+ */
+$nombre = 'Kevin Omar';
+$apellidos = 'Cabanillas Nuñez';
+$edad = '16';
+$estaEnfermo = false;
+$estatura = '1.80';
+$correo_electronico = 'kevinomar377@gmail.com';
+// $4semestre = 'cool'; incorrecto
+// $nombre completo = 'ABC';
