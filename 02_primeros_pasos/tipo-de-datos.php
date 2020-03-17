588df60556d795ebb9c17a00df96033efab58bb8
Kevin Omar Cabanillas Nuñez <kevinomar37@hotmail.com>
2020-03-17 08:45:57 -0700

    Tipos de datos
    Se utilizó la funcion gettype() para obtener el tipo de dato

---
+++ 02_primeros_pasos/tipo-de-datos.php
@@ -0,0 +1,21 @@
+<?php
+//integer, double, string, boolean, array, object, class, unknown type, null
+$entero = 100;
+$decimal = 7.8;
+$cadena = "Kevin";
+$booleano = true;
+$arreglo = array("uno", "dos", "tres");
+class Clase
+{
+    public $id = 1;
+}
+$object = new Clase();
+$nulo = null;
+
+echo '$entero es de tipo: '.gettype($entero)."\n";
+echo '$decimal es de tipo: '.gettype(decimal)."\n";
+echo '$cadena es de tipo: '.gettype($cadena)."\n";
+echo '$booleano es de tipo: '.gettype($booleano)."\n";
+echo '$arreglo es de tipo: '.gettype($arreglo)."\n";
+echo '$object es de tipo: '.gettype($object)."\n";
+echo '$nulo es de tipo: '.gettype($nulo)."\n";
