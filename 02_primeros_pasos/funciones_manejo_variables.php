9fb31ba1d5bf0d8f0a6cc8a55a223ded169e1418
Kevin Omar Cabanillas Nuñez <kevinomar37@hotmail.com>
2020-03-18 13:21:52 -0700

    Funciones de manejo de variables
    Funciones como isset(), empty(), etc

---
+++ 02_primeros_pasos/funciones_manejo_variables.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Otras funciones de manejo de variables
+ * isset(): Determine ig a variables is declared and is different than NULL
+ * return true
+ * empty(): Determine whether a variable is empty
+ * unset(): Unset a given variable
+ * is_integer: Alias of is_int()
+ * is_int(): Find whether the type of a vatiable is integer
+ * is_string(): 
+ * is_double(): 
+ */
+
+ $demo = "demo";
+ if (isset($demo))
+ {
+     echo "La variable \$demo esta declarada y es distinta de NULL"."\n";
+ }
+ else
+ {
+     echo "La variable \$demo no esta declarada o es igual a NULL"."\n";
+ }
+
+
+ // empty == false == null
+ $clean;
+ if (empty($clean))
+ {
+     echo 'La variable \$clean no tiene valor alguno, aun.'."\n";
+ }
+ // TODO: revisar las funciones restantes y agregar ejemplos
