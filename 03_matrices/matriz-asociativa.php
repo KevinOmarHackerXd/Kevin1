68b4fdb250749be9562b4a689a27ac714382c2c3
KevinOmarHackerXd <kevinomar37@hotmail.com>
2020-04-28 18:29:31 -0600

    Práctica con matrices

--- 03_matrices/matriz-asociativa.php
+++ 03_matrices/matriz-asociativa.php
@@ -1,4 +1,10 @@
 <?php
+/*
+Crear una matriz asociativa que contenga los nombres de los siguientes municipios y estados de la república mexicana 
+(Guasave, Sinaloa; Guadalajara, Jalisco; Tepic, Nayarit;
+Culiacan, Sinaloa; Hermosillo, Sonora; Puerto Vallarta, Jalisco; Navojoa, Sonora).
+Utiliza los estados como claves y los municipios como valores;
+*/
 $estado = array (
 "Guasave" => "Sinaloa",
 "Guadalajara" => "Jalisco",
